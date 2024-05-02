b1393947f3605acac56fda199da6a42e5c88a4d5
Skyland Woll <skylandwoll@gmail.com>
2024-05-02 13:14:39 -0400

    SW - start up basic demo site, add default image srcset config

---
+++ site/config/config.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * The config file is optional. It accepts a return array with config options
+ * Note: Never include more than one return statement, all options go within this single return array
+ * In this example, we set debugging to true, so that errors are displayed onscreen. 
+ * This setting must be set to false in production.
+ * All config options: https://getkirby.com/docs/reference/system/options
+ */
+return [
+    'debug' => true,
+    'panel' =>[
+        'install' => true
+    ],
+    'thumbs' => [
+        'srcsets' => [
+            'default' => [
+                '300w'  => ['width' => 300],
+                '600w'  => ['width' => 600],
+                '900w'  => ['width' => 900],
+                '1200w' => ['width' => 1200],
+                '1800w' => ['width' => 1800]
+            ],
+            'square' => [
+                '300w'  => ['width' => 300, 'height' => 300, 'crop' => 'center'],
+                '600w'  => ['width' => 600, 'height' => 600, 'crop' => 'center'],
+                '900w'  => ['width' => 900, 'height' => 900, 'crop' => 'center'],
+                '1200w' => ['width' => 1200, 'height' => 1200, 'crop' => 'center'],
+                '1800w' => ['width' => 1800, 'height' => 1800, 'crop' => 'center']
+            ],
+            // more srcsets as needed
+        ]
+    ]
+];
---
+++ site/snippets/image.php
@@ -0,0 +1,20 @@
+<?php
+
+$attrs = attr([
+  'data-lightbox' => $lightbox ?? false,
+  'href'          => $href     ?? $src,
+]);
+
+?>
+<a <?= $attrs ?>>
+  <img
+    src="<?= esc($src, 'attr') ?>"
+    alt="<?= esc($alt, 'attr') ?>"
+    style="
+      aspect-ratio: <?= $ratio ?? 'auto' ?>;
+      object-fit: <?= ($contain ?? false) ? 'contain' : 'cover' ?>
+    "
+    srcset="<?= esc($srcset ?? '', 'attr') ?>"
+    sizes="<?= esc($sizes ?? '', 'attr') ?>"
+  >
+</a>
